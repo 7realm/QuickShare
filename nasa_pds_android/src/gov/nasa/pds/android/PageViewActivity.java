4e6b56178c35a05433fe1c4fb764272e21297cd9
Taras Kovalchuk <7realm@gmail.com>
2012-04-26 20:21:34 +0300

    Recreated project.

---
+++ nasa_pds_android/src/gov/nasa/pds/android/PageViewActivity.java
@@ -0,0 +1,14 @@
+package gov.nasa.pds.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PageViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_results);
+
+    }
+}
